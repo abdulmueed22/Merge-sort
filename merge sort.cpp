9018bf72f566177d0f5dbd777b1ea9a50f683e71
abdulmueed22 <123588862+abdulmueed22@users.noreply.github.com>
2023-11-20 11:52:25 -0800

    Add files via upload

---
+++ merge sort.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+// Merge function to merge two sorted subarrays
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Create temporary arrays
+    int L[n1], R[n2];
+
+    // Copy data to temporary arrays L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    // Merge the temporary arrays back into arr[left..right]
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// Recursive function to perform merge sort on the array
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        // Same as (left + right) / 2, but avoids overflow for large left and right
+        int mid = left + (right - left) / 2;
+
+        // Sort the first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    const int size = 11;
+    int a[size] = {0, 2, 7, 1, 3, 9, 5, 8, 7, 6, 4};
+
+    // Call the mergeSort function to sort the array
+    mergeSort(a, 0, size - 1);
+
+    // Print the sorted array
+    cout << "Sorted array: ";
+    for (int k = 0; k < size; ++k) {
+        cout << a[k] << " ";
+    }
+
+    return 0;
+}
+
